bc7c2d21b557300ea2bb20f9a6cc3af003665a4a
siuster95 <jxs7205@rit.edu>
2017-11-08 16:50:11 -0500

    fixed up timer

--- src/timer.js
+++ src/timer.js
@@ -4,7 +4,7 @@ const Socket = require('./sockets');
 const changeTime = (timer, lastUpdate, RoomNumber) => {
   const now = Date.now();
   const dt = now - lastUpdate;
-  const newlastUpdate = lastUpdate - now;
+  const newlastUpdate = now;
   const secondsDecrease = dt / 1000;
   const newtimer = timer - secondsDecrease;
   Socket.sendbacktoRoom(RoomNumber, newtimer, newlastUpdate, secondsDecrease);
